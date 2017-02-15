9636821802484d75ec5034161783b09409645aac
Lijun Zhu <gatechzhu@gmail.com>
2017-02-15 18:13:41 -0500

    Change string format to compatible mode.

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ MAJOR = 0
 MINOR = 1
 MICRO = 0
 ISRELEASED = False
-VERSION = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
-
+# VERSION = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 # VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
