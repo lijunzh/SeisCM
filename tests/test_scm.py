5427ccfded106641310b0ebcac134eed3768841e
Lijun Zhu <gatechzhu@gmail.com>
2017-02-15 17:54:03 -0500

    Add test file for bwr

---
+++ tests/test_scm.py
@@ -0,0 +1,13 @@
+import pytest
+
+from scm import bwr
+from matplotlib.colors import LinearSegmentedColormap
+
+
+class TestBwr():
+    def test_alpha_range(self):
+        with pytest.raises(ValueError):
+            bwr(-1)
+
+    def test_output_type(self):
+        assert isinstance(bwr(), LinearSegmentedColormap)
